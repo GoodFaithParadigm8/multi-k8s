1e0bcf81a9c6d4cb8b3f899b1d0bdb6743ab5c08
Jason Sprouse <jason.a.sprouse@gmail.com>
2019-02-19 15:17:58 -0600

    fixed error in docker deploy

--- deploy.sh
+++ deploy.sh
@@ -1,12 +1,12 @@
 docker build -t goodfaith/multi-client:latest -t goodfaith/multi-client:$SHA -f ./client/Dockerfile ./client
 docker build -t goodfaith/multi-server:latest -t goodfaith/multi-server:$SHA -f ./server/Dockerfile ./server
 docker build -t goodfaith/multi-worker:latest -t goodfaith/multi-worker:$SHA -f ./worker/Dockerfile ./worker
-docker push goodfaith good faith/multi-client:latest
-docker push goodfaith good faith/multi-server:latest
-docker push goodfaith good faith/multi-worker:latest
-docker push goodfaith good faith/multi-client:$SHA
-docker push goodfaith good faith/multi-server:$SHA
-docker push goodfaith good faith/multi-worker:$SHA
+docker push goodfaith/multi-client:latest
+docker push goodfaith/multi-server:latest
+docker push goodfaith/multi-worker:latest
+docker push goodfaith/multi-client:$SHA
+docker push goodfaith/multi-server:$SHA
+docker push goodfaith/multi-worker:$SHA
 kubectl apply -f k8s
 kubectl set image deployments/server-deployment server=goodfaith/multi-server:$SHA
 kubectl set image deployments/client-deployment client=goodfaith/multi-client:$SHA
